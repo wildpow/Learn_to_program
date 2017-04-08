333d5df30b171b595c3826fd5e2827a28e0cca01
Aaron Wilder <element1028@gmail.com>
2017-04-08 12:14:31 -0700

    updated input check
    added a method to check if input is a string and not a number/blank/nil

--- chapter_5/full_name.rb
+++ chapter_5/full_name.rb
@@ -1,8 +1,16 @@
+def input_check(var, name_type)
+  while var == nil || var == "" || var.to_i != 0
+    puts "What is your #{name_type} name again?"
+    var = gets.chomp.capitalize
+  end
+  var
+end
+
 puts "What is your first name?"
-first_name = gets.chomp.capitalize
+first_name = input_check(gets.chomp,"first")
 puts "and your middle name?"
-middle_name = gets.chomp.capitalize
+middle_name = input_check(gets.chomp,"middle")
 puts "and finally your last name?"
-last_name = gets.chomp.capitalize
+last_name = input_check(gets.chomp,"last")
 
 puts "Welcome #{first_name} #{middle_name} #{last_name} to the book Learn to program by Chris Pine."
