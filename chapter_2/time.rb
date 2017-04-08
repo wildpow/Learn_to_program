882b36a81dfad0c43f343d3c6b031d21f99faba0
Aaron Wilder <element1028@gmail.com>
2017-04-07 20:01:55 -0700

    finished chapter 2
    not 100% accurate yet

--- chapter_2/time.rb
+++ chapter_2/time.rb
@@ -1,8 +1,9 @@
 YEARS_IN_DECADE = 10
 DAYS_IN_YEAR = 365
 MONTHS_IN_YEAR = 12
-HOURS_IN_YEAR = DAYS_IN_YEAR * HOURS_IN_DAY
 HOURS_IN_DAY = 24
+HOURS_IN_YEAR = DAYS_IN_YEAR * HOURS_IN_DAY
+
 MINUTES_IN_HOUR = 60
 SECONDS_IN_HOUR = 60
 SECONDS_IN_MINUTE = 60
@@ -30,14 +31,16 @@ def days_so_far(days_in_month)
 end
 
 hours_passed_this_year = days_so_far(DAYS_IN_MONTH) * HOURS_IN_DAY + the_hour
+minutes_passed_this_year = hours_passed_this_year * MINUTES_IN_HOUR + time.min
+
 my_age_in_days = (this_year - my_birth_year - 1) * DAYS_IN_YEAR + days_so_far(DAYS_IN_MONTH)
-my_age_in_hours = my_age_in_days * HOURS_IN_DAY + hours_passed_this_year
+my_age_in_hours = my_age_in_days * 24 + hours_passed_this_year
 my_age_in_minutes = my_age_in_hours * MINUTES_IN_HOUR + time.min
 my_age_in_seconds = my_age_in_minutes * SECONDS_IN_MINUTE + time.sec
 
-minutes_passed_this_year = hours_passed_this_year *
 
-
+chris_pines_age = 1025000000 / SECONDS_IN_MINUTE / MINUTES_IN_HOUR / HOURS_IN_DAY / DAYS_IN_YEAR
+chris_pines_age_when_he_started_this_book = 800000000 / SECONDS_IN_MINUTE / MINUTES_IN_HOUR / HOURS_IN_DAY / DAYS_IN_YEAR
 
 puts "There are #{HOURS_IN_YEAR} hours in a year!"
 puts "and #{MINUTES_IN_DECADE} minutes in a decade."
@@ -49,3 +52,7 @@ puts "or #{my_age_in_days} days old..."
 puts "or #{my_age_in_hours} hours old..."
 puts "or #{my_age_in_minutes} minutes old.."
 puts "or #{my_age_in_seconds} seconds old..."
+puts "well close enough..."
+
+puts "Chris Pine started this book at age #{chris_pines_age_when_he_started_this_book}"
+puts "and finished it when he was #{chris_pines_age}"
