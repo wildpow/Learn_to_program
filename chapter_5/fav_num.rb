aabaac24265d421b0521aa08475400c5dd885251
Aaron Wilder <element1028@gmail.com>
2017-04-08 12:12:40 -0700

    chapter 5 exercises
    page 30

---
+++ chapter_5/fav_num.rb
@@ -0,0 +1,4 @@
+puts "What's your favorite number?"
+input = gets.to_i
+puts "I know your favorite number is #{input}.."
+puts "but, wouldn't be better if it was a bigger number like #{input + 1}!!!!!!"
---
+++ chapter_5/full_name.rb
@@ -0,0 +1,8 @@
+puts "What is your first name?"
+first_name = gets.chomp.capitalize
+puts "and your middle name?"
+middle_name = gets.chomp.capitalize
+puts "and finally your last name?"
+last_name = gets.chomp.capitalize
+
+puts "Welcome #{first_name} #{middle_name} #{last_name} to the book Learn to program by Chris Pine."
