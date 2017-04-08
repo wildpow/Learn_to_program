b69c9ae23e83531f5427aa510c6ca24af7f92b33
Aaron Wilder <element1028@gmail.com>
2017-04-08 12:33:03 -0700

    added name length
    Chris asked me to on page 36

--- chapter_5/full_name.rb
+++ chapter_5/full_name.rb
@@ -1,9 +1,9 @@
 def input_check(var, name_type)
   while var == nil || var == "" || var.to_i != 0
     puts "What is your #{name_type} name again?"
-    var = gets.chomp.capitalize
+    var = gets.chomp
   end
-  var
+  var.capitalize
 end
 
 puts "What is your first name?"
@@ -13,4 +13,6 @@ middle_name = input_check(gets.chomp,"middle")
 puts "and finally your last name?"
 last_name = input_check(gets.chomp,"last")
 
+name_length = first_name.length + middle_name.length + last_name.length
 puts "Welcome #{first_name} #{middle_name} #{last_name} to the book Learn to program by Chris Pine."
+puts "Also your full name has #{name_length} characters in it"
